97d0007028cd03ab8c8a96927489b0cf9845b244
gchitic <giulietac10@gmail.com>
2024-02-01 19:34:09 +0200

    my first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace MathQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTimeLeft = new Label();
+            textBoxTimeLeft = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            lblPlusLeft = new Label();
+            lblPlus = new Label();
+            lblPlusRight = new Label();
+            lblEqual = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            lblMinus = new Label();
+            label4 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            label5 = new Label();
+            label6 = new Label();
+            lblMultiply = new Label();
+            label8 = new Label();
+            numericUpDown3 = new NumericUpDown();
+            label9 = new Label();
+            label10 = new Label();
+            lblDevide = new Label();
+            label12 = new Label();
+            numericUpDown4 = new NumericUpDown();
+            btnStart = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTimeLeft
+            // 
+            lblTimeLeft.AutoSize = true;
+            lblTimeLeft.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblTimeLeft.Location = new Point(495, 9);
+            lblTimeLeft.Name = "lblTimeLeft";
+            lblTimeLeft.Size = new Size(108, 31);
+            lblTimeLeft.TabIndex = 0;
+            lblTimeLeft.Text = "Time Left";
+            // 
+            // textBoxTimeLeft
+            // 
+            textBoxTimeLeft.Location = new Point(609, 9);
+            textBoxTimeLeft.Name = "textBoxTimeLeft";
+            textBoxTimeLeft.Size = new Size(99, 27);
+            textBoxTimeLeft.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            numericUpDown1.Location = new Point(454, 65);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 38);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // lblPlusLeft
+            // 
+            lblPlusLeft.AutoSize = true;
+            lblPlusLeft.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblPlusLeft.Location = new Point(99, 57);
+            lblPlusLeft.Name = "lblPlusLeft";
+            lblPlusLeft.Size = new Size(35, 46);
+            lblPlusLeft.TabIndex = 3;
+            lblPlusLeft.Text = "?";
+            // 
+            // lblPlus
+            // 
+            lblPlus.AutoSize = true;
+            lblPlus.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblPlus.Location = new Point(197, 57);
+            lblPlus.Name = "lblPlus";
+            lblPlus.Size = new Size(43, 46);
+            lblPlus.TabIndex = 4;
+            lblPlus.Text = "+";
+            // 
+            // lblPlusRight
+            // 
+            lblPlusRight.AutoSize = true;
+            lblPlusRight.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblPlusRight.Location = new Point(305, 57);
+            lblPlusRight.Name = "lblPlusRight";
+            lblPlusRight.Size = new Size(35, 46);
+            lblPlusRight.TabIndex = 5;
+            lblPlusRight.Text = "?";
+            // 
+            // lblEqual
+            // 
+            lblEqual.AutoSize = true;
+            lblEqual.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblEqual.Location = new Point(387, 57);
+            lblEqual.Name = "lblEqual";
+            lblEqual.Size = new Size(43, 46);
+            lblEqual.TabIndex = 6;
+            lblEqual.Text = "=";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(387, 145);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 46);
+            label1.TabIndex = 11;
+            label1.Text = "=";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(305, 145);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 46);
+            label2.TabIndex = 10;
+            label2.Text = "?";
+            // 
+            // lblMinus
+            // 
+            lblMinus.AutoSize = true;
+            lblMinus.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblMinus.Location = new Point(197, 145);
+            lblMinus.Name = "lblMinus";
+            lblMinus.Size = new Size(34, 46);
+            lblMinus.TabIndex = 9;
+            lblMinus.Text = "-";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(99, 145);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 46);
+            label4.TabIndex = 8;
+            label4.Text = "?";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            numericUpDown2.Location = new Point(454, 153);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(150, 38);
+            numericUpDown2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(387, 229);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 46);
+            label5.TabIndex = 16;
+            label5.Text = "=";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label6.Location = new Point(305, 229);
+            label6.Name = "label6";
+            label6.Size = new Size(35, 46);
+            label6.TabIndex = 15;
+            label6.Text = "?";
+            // 
+            // lblMultiply
+            // 
+            lblMultiply.AutoSize = true;
+            lblMultiply.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblMultiply.Location = new Point(197, 229);
+            lblMultiply.Name = "lblMultiply";
+            lblMultiply.Size = new Size(34, 46);
+            lblMultiply.TabIndex = 14;
+            lblMultiply.Text = "*";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label8.Location = new Point(99, 229);
+            label8.Name = "label8";
+            label8.Size = new Size(35, 46);
+            label8.TabIndex = 13;
+            label8.Text = "?";
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            numericUpDown3.Location = new Point(454, 237);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(150, 38);
+            numericUpDown3.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label9.Location = new Point(387, 313);
+            label9.Name = "label9";
+            label9.Size = new Size(43, 46);
+            label9.TabIndex = 21;
+            label9.Text = "=";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label10.Location = new Point(305, 313);
+            label10.Name = "label10";
+            label10.Size = new Size(35, 46);
+            label10.TabIndex = 20;
+            label10.Text = "?";
+            // 
+            // lblDevide
+            // 
+            lblDevide.AutoSize = true;
+            lblDevide.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            lblDevide.Location = new Point(197, 313);
+            lblDevide.Name = "lblDevide";
+            lblDevide.Size = new Size(33, 46);
+            lblDevide.TabIndex = 19;
+            lblDevide.Text = "/";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label12.Location = new Point(99, 313);
+            label12.Name = "label12";
+            label12.Size = new Size(35, 46);
+            label12.TabIndex = 18;
+            label12.Text = "?";
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            numericUpDown4.Location = new Point(454, 321);
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(150, 38);
+            numericUpDown4.TabIndex = 17;
+            // 
+            // btnStart
+            // 
+            btnStart.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            btnStart.Location = new Point(227, 384);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(171, 43);
+            btnStart.TabIndex = 22;
+            btnStart.Text = "Start the quiz";
+            btnStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnStart);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(lblDevide);
+            Controls.Add(label12);
+            Controls.Add(numericUpDown4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(lblMultiply);
+            Controls.Add(label8);
+            Controls.Add(numericUpDown3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(lblMinus);
+            Controls.Add(label4);
+            Controls.Add(numericUpDown2);
+            Controls.Add(lblEqual);
+            Controls.Add(lblPlusRight);
+            Controls.Add(lblPlus);
+            Controls.Add(lblPlusLeft);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBoxTimeLeft);
+            Controls.Add(lblTimeLeft);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTimeLeft;
+        private TextBox textBoxTimeLeft;
+        private NumericUpDown numericUpDown1;
+        private Label lblPlusLeft;
+        private Label lblPlus;
+        private Label lblPlusRight;
+        private Label lblEqual;
+        private Label label1;
+        private Label label2;
+        private Label lblMinus;
+        private Label label4;
+        private NumericUpDown numericUpDown2;
+        private Label label5;
+        private Label label6;
+        private Label lblMultiply;
+        private Label label8;
+        private NumericUpDown numericUpDown3;
+        private Label label9;
+        private Label label10;
+        private Label lblDevide;
+        private Label label12;
+        private NumericUpDown numericUpDown4;
+        private Button btnStart;
+    }
+}
