af24580a6ab608da9d876bf338f3d35a15d5bc4a
gchitic <giulietac10@gmail.com>
2024-02-01 19:47:52 +0200

    test

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,8 @@ namespace MathQuiz
 {
     public partial class Form1 : Form
     {
+        Random randomizer = new Random();
+        int addend1, addend2;
         public Form1()
         {
             InitializeComponent();
